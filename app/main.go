da9a1db37341fc93267eed862cf67632a253cb05
Syafa Hadyan Rasendriya <syafa@syafahadyan.com>
2026-03-06 08:27:54 +0700

    feat: bootstrap

--- app/main.go
+++ app/main.go
@@ -2,8 +2,34 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/SyafaHadyan/worku/internal/bootstrap"
 )
 
 func main() {
-	fmt.Println("app started")
+	app := bootstrap.Start()
+
+	go func() {
+		err := app.App.Fiber.Listen(fmt.Sprintf(":%d", app.Config.AppPort))
+		if err != nil {
+			log.Panic(err)
+		}
+	}()
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+
+	<-c
+	log.Println("gracefully shutting down")
+
+	err := app.App.Fiber.Shutdown()
+	if err != nil {
+		log.Println("graceful shutdown failed")
+	}
+
+	log.Println("shutdown complete")
 }
